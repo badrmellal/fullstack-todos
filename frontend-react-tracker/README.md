4946dca7edae16f2fe56ac7301094aed9bcc891c
badr <132598492+badrmellal@users.noreply.github.com>
2023-12-24 15:05:59 +0100

    Add files via upload

---
+++ frontend-react-tracker/README.md
@@ -0,0 +1,19 @@
+# Getting Started with Create React App
+
+This application is connected to backend server built on Spring Boot.
+
+## Available Scripts
+
+In the project directory, you can run:
+
+### `npm install`
+
+then run
+
+### `npm start`
+
+Runs the app in the development mode.\
+Open [http://localhost:3000](http://localhost:3000) to view it in your browser.
+
+The page will reload when you make changes.\
+You may also see any lint errors in the console.
---
+++ frontend-react-tracker/src/components/AddTask.jsx
@@ -0,0 +1,50 @@
+import { useState } from "react"
+
+const AddTask = ({ onAdd }) => {
+
+    const [username, setUsername] = useState('');
+    const [tasks, setTasks] = useState('');
+    const [dayAndTime, setDayAndTime] = useState('');
+    const [reminder, setReminder] = useState(false);
+
+    const onSubmit = (e) => {
+        e.preventDefault();
+
+        if (!tasks || !dayAndTime || !username) {
+            alert(`Please add ${!tasks ? 'Task' : !dayAndTime ? 'Day & Time' : 'Username'}`);
+            return;
+        }
+
+        onAdd({username, tasks, dayAndTime, reminder });
+
+        setUsername('');
+        setTasks('');
+        setDayAndTime('');
+        setReminder(false);
+
+    }
+
+  return (
+    <form className="add-form" onSubmit={onSubmit}>
+        <div className="form-control">
+            <label>Username</label>
+            <input type="text" placeholder="Add Username" value={username} onChange={(e)=> setUsername(e.target.value)}/>
+        </div>
+        <div className="form-control">
+            <label>Task</label>
+            <input type="text" placeholder="Add Task" value={tasks} onChange={(e)=> setTasks(e.target.value)}/>
+        </div>
+        <div className="form-control">
+            <label>Day & Time</label>
+            <input type="text" placeholder="Add Day & Time" value={dayAndTime} onChange={(e)=> setDayAndTime(e.target.value)}/>
+        </div>
+        <div className="form-control form-control-check">
+            <label>Set Reminder</label>
+            <input type="checkbox" checked={reminder} value={reminder} onChange={(e)=> setReminder(e.currentTarget.checked)}/>
+        </div>
+        <input type="submit" value='Save Task' className="btn btn-block"/>
+    </form>
+  )
+}
+
+export default AddTask;
